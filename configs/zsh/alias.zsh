566fc062f14d36c0a3c9463909c7d4eaba5cc37a
Ashish Patel <ashishsushilpatel@gmail.com>
2021-12-30 06:08:09 +0000

    feat(zsh): add alias for programming problems

--- configs/zsh/alias.zsh
+++ configs/zsh/alias.zsh
@@ -1,6 +1,11 @@
+# Common alias
 alias dockerbuild="docker-compose up -d --no-deps --build"
 alias code='cd ~/Code'
 alias dotfiles='cd ~/Code/dotfiles'
 alias q="cd ~ && clear"
 alias desktop='cd ~/Desktop'
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
+
+# Programming problems alias
+alias lc='cd ~/Code/personal/code.ashish.me/leetcode'
+alias gfg='cd ~/Code/personal/code.ashish.me/geeksforgeeks'
