72a700dc61805a7b6fd2dd29caf5fda97b61e43c
Ashish Patel <ashishsushilpatel@gmail.com>
2021-12-30 06:09:31 +0000

    feat(zsh): update alias

--- configs/zsh/alias.zsh
+++ configs/zsh/alias.zsh
@@ -7,5 +7,5 @@ alias desktop='cd ~/Desktop'
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
 
 # Programming problems alias
-alias lc='cd ~/Code/personal/code.ashish.me/leetcode'
-alias gfg='cd ~/Code/personal/code.ashish.me/geeksforgeeks'
+alias leetcode='cd ~/Code/personal/code.ashish.me/leetcode'
+alias geeksforgeeks='cd ~/Code/personal/code.ashish.me/geeksforgeeks'
