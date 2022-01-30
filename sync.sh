fd37286e9cb692995a6783fef6c390bad546b0c1
Ashish Patel <ashishsushilpatel@gmail.com>
2022-01-30 05:19:09 +0000

    feat(code): add editor config

--- sync.sh
+++ sync.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# misc
+ln -sf ~/code/dotfiles/configs/.editorconfig ~/.editorconfig
+
 # zsh
 ln -sf ~/code/dotfiles/configs/zsh/.zshrc ~/.zshrc
 ln -sf ~/code/dotfiles/configs/zsh/alias.zsh ~/alias.zsh
@@ -9,3 +12,5 @@ ln -sf ~/code/dotfiles/configs/starship/starship.toml ~/.starship/starship.toml
 sudo ln -sf ~/code/dotfiles/etc/hosts /etc/hosts
 
 #espanso
+ln -sf ~/code/dotfiles/configs/espanso/common.yml /Users/ashish/Library/Preferences/espanso/user/common.yml
+ln -sf ~/code/dotfiles/configs/espanso/utils.yml /Users/ashish/Library/Preferences/espanso/user/utils.yml
