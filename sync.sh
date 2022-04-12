df7cd4e1ba0b9aa8492aff44413acc984d25765c
Ashish Patel <ashishsushilpatel@gmail.com>
2022-04-12 20:50:26 +0100

    feat(npm): add npmrc

--- sync.sh
+++ sync.sh
@@ -5,6 +5,7 @@ ln -sf ~/code/dotfiles/welcome.sh ~/utils/welcome.sh
 ln -sf ~/code/dotfiles/.editorconfig ~/.editorconfig
 ln -sf ~/code/dotfiles/.gitignore ~/.gitignore
 ln -sf ~/code/dotfiles/.ignore ~/.ignore
+ln -sf ~/code/dotfiles/.npmrc ~/.npmrc
 
 # zsh
 ln -sf ~/code/dotfiles/configs/neofetch/config.conf ~/.config/neofetch/config.conf
